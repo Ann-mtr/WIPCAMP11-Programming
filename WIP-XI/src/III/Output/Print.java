156ed12999bc12addf606ed82e26ac7de899b7e9
Ann-mtr <montharag@gmail.com>
2019-05-30 11:37:30 +0700

    [ADD] New Project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,22 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("แอน \n");
+        
+        System.out.print("รสซากุระ \n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("ตอง บั๊มพ์");
+        
+               
+    }
+}
